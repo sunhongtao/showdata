ab21380488c7e308f7de688e62d8e9adc83876d2
hyy499 <691795636@qq.com>
2017-08-09 12:47:40 +0800

    second commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from . import main
-from flask import render_template
+from flask import render_template, Response, json
 import pymysql
 
 
@@ -22,3 +22,14 @@ def get_warning_level(sql_info):
     finally:
         db.close()
     return result
+
+
+@main.route('/ajax/getIP', methods=['GET'])
+def get_ip(request):
+    level = request.args.get('level')
+    sql_info = 'SELECT DISTINCT ip from icinga_info WHERE LEVEL = {}'.format(level)
+    ip = get_warning_level(sql_info)
+    print(ip)
+    result = {}
+    result["ip"] = ip
+    return Response(json.dumps(), content_type='application/json')
